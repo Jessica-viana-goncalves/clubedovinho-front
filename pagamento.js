f1ce2ce46d728b6835d51669ce4dd20235c68579
unknown <jessicagoncalves0435@digitalcollege.internal>
2025-02-06 21:48:53 -0300

    tela de pagamento

--- pagamento.js
+++ pagamento.js
@@ -3,54 +3,99 @@ const INPUT_CVV = document.getElementById('card_cvv');
 const INPUT_TITULAR = document.getElementById('card_titular');
 const SELECT_MES = document.getElementById('card_mes');
 const SELECT_ANO = document.getElementById('card_ano');
+const SPAN_CVV = document.getElementById('cartao_verso')
 
 let hoje = new Date();
-let anoFim = hoje.getFullYear() + 10; 
+let anoFim = hoje.getFullYear() + 10;
 
-// Preenchendo as opções de ano
 for (let ano = hoje.getFullYear(); ano <= anoFim; ano++) {
-    SELECT_ANO.innerHTML += `<option value="${ano}">${ano}</option>`;  
+    SELECT_ANO.innerHTML += `<option>${ano}</option>`;
 }
 
 const MESES = [
-    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
-    'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
 ];
 
-// Preenchendo as opções de mês
 for (let mes = 0; mes <= 11; mes++) {
-    SELECT_MES.innerHTML += `<option value="${mes + 1}">${MESES[mes]}</option>`;
+    SELECT_MES.innerHTML += `<option>${MESES[mes]}</option>`;
 }
-
 const SPAN_NUMERO = document.getElementById('resultado_numero');
 const SPAN_TITULAR = document.getElementById('resultado_titular');
-const SPAN_MES = document.getElementById('resultado_mes');
 const SPAN_ANO = document.getElementById('resultado_ano');
+const SPAN_MES = document.getElementById('resultado_mes');
 
-function preencherNumero() {
-    SPAN_NUMERO.innerHTML = INPUT_NUMERO.value;
+function removerUltimoDigito (numero) {
+    return numero.substr(0, numero.length - 1);
 }
 
+function preencherNumero() {
+    let digitos = INPUT_NUMERO.value;
+    
+    if (isNaN (digitos)) {
+        digitos = digitos.replace(/\D/g, '') 
+    }
+
+    digitos = digitos.replace(/(\d{4})(?=\d)/g, '$1 ');
+    
+    INPUT_NUMERO.value = digitos;
+    SPAN_NUMERO.innerHTML = digitos;
+
+   if (INPUT_NUMERO.value.length > 1) {
+    return;
+   }
+
+document.getElementById ('bandeira_visa').style.display = 'none'; 
+document.getElementById ('bandeira_mastercard').style.display = 'none';
+
+if (INPUT_NUMERO.value === '4') {
+    document.getElementById ('bandeira_visa').style.display = 'block';
+   }
+
+   if (INPUT_NUMERO.value === '5') {
+    document.getElementById ('bandeira_mastercard').style.display = 'block';
+   }
+
+   SPAN_NUMERO.innerHTML = INPUT_NUMERO.value;
+
+}
 function preencherTitular() {
     SPAN_TITULAR.innerHTML = INPUT_TITULAR.value;
 }
-
-function preencherMes() {
-    const mesSelecionado = SELECT_MES.value;
-    SPAN_MES.innerHTML = mesSelecionado ? mesSelecionado : 'MM';
+function selecionarAno() {
+    SPAN_ANO.innerHTML = SELECT_ANO.value
+}
+function selecionarMes() {
+    let m = SELECT_MES.selectedIndex;
+    if (m < 10) {
+        SPAN_MES.innerHTML = "0"+m;
+    } else {
+        SPAN_MES.innerHTML = m;
+    }
 }
 
-function preencherAno() {
-    const anoSelecionado = SELECT_ANO.value;
-    SPAN_ANO.innerHTML = anoSelecionado ? anoSelecionado : 'YYYY';
+function preencherCVV () {
+    SPAN_CVV.innerHTML = INPUT_CVV.value;
 }
 
-INPUT_NUMERO.addEventListener('input', preencherNumero);
-INPUT_TITULAR.addEventListener('input', preencherTitular);
+function mostrarFrente() {
 
 
-if (m < 10) {
-    SPAN_MES.innerHTML = "0"+m;
-} else {
-    SPAN_MES.innerHTML = m;
+    document.getElementById('cartao_frente').style.display = 'block';
+    document.getElementById('cartao_verso').style.display = 'none';
+}
+
+function mostrarVerso() {
+    document.getElementById('cartao_verso').style.display = 'block';
+    document.getElementById('cartao_frente').style.display = 'none';
 }
